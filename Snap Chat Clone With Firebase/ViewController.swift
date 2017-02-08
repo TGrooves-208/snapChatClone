6de94fd0342b004e38881aadaa2192ef5b73ed89
TGrooves-208 <tgrooves.208@gmail.com>
2017-02-07 19:47:03 -0700

    Initial Commit

---
+++ Snap Chat Clone With Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snap Chat Clone With Firebase
+//
+//  Created by Gil Aguilar on 2/7/17.
+//  Copyright © 2017 Red Eye Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
